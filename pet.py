3fc4492ba27d06d06121a9c20a4b2a7d083aa748
AveryBau <averya34@nycstudents.net>
2026-03-23 10:13:59 -0400

    Update pet.py

--- pet.py
+++ pet.py
@@ -58,9 +58,9 @@ class Pet:
     def show_status(self):
         print(f"{self.name} is {self.__happiness}")
 
-Dog = Pet("Bark Bark", 1)
-Dog.play(play = 9)
-print(Dog.__dict__)
+Doggy = Pet("Bark Bark", 1)
+Doggy.play(play = 9)
+print(Doggy.__dict__)
 
 class Hero:
     def __init__(self, name, money, inventory):
@@ -79,3 +79,53 @@ Sabrina.spend("MORE YURI")
 print(Sabrina.__dict__)
 
 
+class User: 
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    def display_info(self):
+        return f"User: {self.name}, Email: {self.email}"
+
+class Student(User):
+    def __init__(self, name, email, student_id):
+        super().__init__(name, email)
+        self.student_id = student_id
+    
+    def display_info(self):
+        return f"Student: {self.name}, Email:{self.email}, Student ID: {self.student_id}"
+    
+class Teacher(User):
+    def __init__(self, name, email, subject):
+        super().__init__(name, email)
+        self.subject = subject
+    
+    def display_info(self):
+        base_info = super().display_info()
+        return f"{base_info}, Subject: {self.subject}"
+        return f"teacher: {self.name}, Email: {self.email}, Subject: {self.subject}"
+    
+class Administrator(User):
+    def __init__(self, name, email, role):
+        super().__init__(name, email)
+        self.role = role
+
+    def display_info(self):
+        return f"Administrator: {self.name}, Email: {self.email}, Role: {self.role}"
+    
+    def manage_system(self):
+        return f"{self.name} ({self.role}) is managing the vote"
+    
+
+student = Student("Alice", "alice@example.com", "S12345")
+teacher = Teacher("Mr. Smith", "smith@example.com", "Mathematics")
+administrator = Administrator("Ms. Johnson", "johnson@example.com", "Principal")
+
+print(student.display_info())
+print(teacher.display_info())
+print(administrator.display_info())
+
+admin = Administrator("Ms. Johnson", "johnson@example.com", "Principal")
+print(admin.manage_system())
+
+my_teacher = Teacher("Mr. Smith", "smith@example.com", "Mathematics")
+print(my_teacher.display_info())
