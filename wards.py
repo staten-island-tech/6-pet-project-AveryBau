0db2100f265328f2c28330b6e100d32e759f916e
AveryBau <averya34@nycstudents.net>
2026-04-13 09:36:28 -0400

    Update wards.py

--- wards.py
+++ wards.py
@@ -7,15 +7,13 @@ wards = {
 
 def person(ward):
     staff = {}
-    for dept, docs in ward.items():    # for the items in the wards
-       depts = []
-       depts.append(dept)             
+    for dept, docs in ward.items():    # for the items in the wards             
        for doc in docs:                # in all the doctors
            if doc not in staff:        # if the doctors are not in the dictionary
                staff[doc] = [dept]     # format of the dictionary, the doctor = [list of jobs]
            else:   # when it is in the dictionary
-               staff[doc].append(dept)  # add all the departments
-    print(staff)
+               staff[doc].append(dept)  # add all the occupations into the list by adding the depts
+    print(staff["Bob"]) # print the new dictionary
 person(wards)
 
 
